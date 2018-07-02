e1425d5cc23eba22189a42092385ede9f8e4415f
ruber <ruber54@gmail.com>
2018-07-02 18:48:16 +0700

    work on bugs

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
-import ru.javawebinar.topjava.model.Meal;
+
 import ru.javawebinar.topjava.model.MealWithExceed;
 import ru.javawebinar.topjava.util.MealsUtil;
 
@@ -11,9 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.Arrays;
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -23,22 +21,10 @@ public class MealServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        List<Meal> meals = Arrays.asList(
-                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
-                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
-        );
-
-        List<MealWithExceed> mealsWithExceed = MealsUtil.getFilteredWithExceededInOnePass2( meals, 2000, false );
-        request.setAttribute( "meals", mealsWithExceed );
-        RequestDispatcher rd = request.getRequestDispatcher( "meals.jsp" );
-        rd.forward( request, response );
+        List<MealWithExceed> mealsWithExceed = MealsUtil.getFilteredWithExceeded(MealsUtil.meals,  LocalTime.MIN, LocalTime.MAX, 2000);
+        request.setAttribute("meals", mealsWithExceed);
+        RequestDispatcher rd = request.getRequestDispatcher("meals.jsp");
+        rd.forward(request, response);
         log.debug("redirect to users");
-
-//        request.getRequestDispatcher("/users.jsp").forward(request, response);
-        //response.sendRedirect("users.jsp");
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -7,22 +7,16 @@
 </head>
 <body>
 <table>
-    <c:set var="meals" value="${requestScope.meals}" />
+    <c:set var="meals" value="${requestScope.meals}"/>
     <c:forEach items="${meals}" var="meal">
-
-        <c:set var="color" value="#ff0000" />
-        <c:if test="${meal.exceed}">
-            <c:set var="color" value="#008000" />
-        </c:if>
-
+        <c:set var="color" value="${meal.exceed ? '#ff0000' : '#008000'}"/>
         <tr bgcolor=${color}>
-            <javatime:format value="${meal.dateTime}" pattern="yyyy-MM-dd hh:mm" var="parsedDate" />
+            <javatime:format value="${meal.dateTime}" pattern="yyyy-MM-dd hh:mm" var="parsedDate"/>
             <td><c:out value="${parsedDate}"/></td>
             <td><c:out value="${meal.description}"/></td>
             <td><c:out value="${meal.calories}"/></td>
         </tr>
     </c:forEach>
 </table>
-
 </body>
 </html>
